fce6e36bb4e9085a62cd537a4ec3b88695f90e10
Rohan Gupta <guptarohan2002@gmail.com>
2024-06-06 13:19:21 +0530

    clerk setup

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://premium-dragon-29.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default authConfig;
